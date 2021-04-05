335c950f29140d827cb69a7b26bac3a4c7e84159
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-04-05 16:39:34 +0200

    Tutorial compleated

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
 const express = require("express");
-
 const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-app.listen(3000);
+const blogRoutes = require("./routes/blogRoutes");
+
+//connect to mongo DB
+const dbUIR =
+  "mongodb+srv://AdamowiczPiotr:4y8uTDJLCTr7PRP@cluster0.jxr7f.mongodb.net/blogsData?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbUIR, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
+
+//register view engine
+
+//.set first argument is the option we are Setting
+app.set("view engine", "ejs");
+
+//you can set directory for views like so: app.set('views', 'path-to-views-directory')
+
+//middleware & static files
+
+app.use(express.static("styles"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+//routes
 
 app.get("/", (req, res) => {
-  //second argument sets root directory to the current dir cause .sendFile uses absolute path by default
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.redirect("/blogs");
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", {
+    title: "About Page",
+  });
 });
 
-//redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
+//blog routes
+
+app.use("/blogs", blogRoutes);
 
 //error - must by last because .use fires every time
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", {
+    title: "Blog not found",
+  });
 });
